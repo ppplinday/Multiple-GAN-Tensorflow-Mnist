080e7eac11bf765ab851f263008429659c515eec
ppplinday <peilinzhou666@gmail.com>
2018-02-01 17:20:39 -0800

    dcgan v1.2

--- dcgan.py
+++ dcgan.py
@@ -6,6 +6,7 @@ import config
 import itertools
 import scipy.misc
 import PIL
+from PIL import Image
 import numpy as np
 from glob import glob
 import tensorflow as tf
@@ -115,7 +116,7 @@ def merge(images, size):
 
 # save merge picture
 def save_images(images, size, image_path):
-	images = (images * 0.5) + 0.5 
+	images = images
 	img = merge(images, size)
 	return scipy.misc.imsave(image_path, (255*img).astype(np.uint8))
 
@@ -202,7 +203,7 @@ class DCGAN:
 		sample_images = sample_images.reshape(-1, 28, 28, 1)
 		sample_images = tf.image.resize_images(sample_images, [64, 64]).eval()
 		sample_images = sample_images.reshape(-1, 64, 64, 1)
-		sample_images = (sample_images - 0.5) * 2.0
+		#sample_images = (sample_images - 0.5) * 2.0
 		start_time = time.time()
 
 		# load check point
@@ -217,7 +218,7 @@ class DCGAN:
 			batch_images, batch_labels =  mnist.train.next_batch(self.batch_size)
 			batch_images = batch_images.reshape(-1, 28, 28, 1)
 			batch_images = tf.image.resize_images(batch_images, [64, 64]).eval()
-			batch_images = (batch_images - 0.5) * 2.0
+			
 			batch_z = np.random.uniform(-1, 1, [self.batch_size, self.z_dim]).astype(np.float32)
 			# before = batch_images.reshape(-1, 28, 28, 1)
 			# print(before[0])
@@ -241,7 +242,7 @@ class DCGAN:
 			print("Epoch: [{:4d}/{:4d}] time: {:4.4f}, d_loss: {:.8f}, g_loss: {:.8f}".format(
 					id, 12000, time.time() - start_time, err_d, err_g))
 
-			if id % 10 == 0:
+			if id % 1 == 0:
 				samples, d_loss, g_loss = self.sess.run(
 					[self.G, self.d_loss, self.g_loss],
 					feed_dict={self.z: sample_z, self.images: sample_images}
